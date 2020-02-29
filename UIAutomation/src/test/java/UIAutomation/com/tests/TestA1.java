5ae021461139d7483471cea658613d590bb3a858
skumar <santoshkumar_201@yahoo.com>
2020-02-28 22:59:05 -0800

    Java Selenium TestNG Automation Project

---
+++ UIAutomation/src/test/java/UIAutomation/com/tests/TestA1.java
@@ -0,0 +1,19 @@
+package UIAutomation.com.tests;
+
+import org.testng.annotations.Test;
+
+import UIAutomation.utilities.BaseClass;
+
+@Test
+public class TestA1 extends BaseClass{
+	
+	public void Test1() {
+		driver.navigate().to("http://www.google.com");
+	}
+	
+
+	
+	
+	
+
+}
---
+++ UIAutomation/src/test/java/UIAutomation/utilities/BaseClass.java
@@ -0,0 +1,47 @@
+package UIAutomation.utilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+
+
+public class BaseClass {
+	public DesiredCapabilities desiredcapabilities ;
+	public static WebDriver driver;
+		
+	@BeforeTest
+	@Parameters({"browser","gridUrl"})
+	public void init(@Optional("chrome")String browser,@Optional("http://localhost:4444")String gridUrl) throws MalformedURLException{
+		
+		if(browser.equalsIgnoreCase("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--start-maximized");
+			options.addArguments("--ignore-certification-errors");
+			options.addArguments("--disable-popup-blocking");
+			desiredcapabilities = DesiredCapabilities.chrome();
+			desiredcapabilities.setCapability(ChromeOptions.CAPABILITY, options);
+			driver = new RemoteWebDriver(new URL(gridUrl),desiredcapabilities);
+			driver.navigate().to("https://yahoo.com");
+			
+		}else if (browser.equalsIgnoreCase("firefox")) {
+			
+		}else if (browser.equalsIgnoreCase("ie")) {
+			
+		}
+		
+	}
+	
+	@AfterTest
+	public void quit() {
+		driver.close();
+	}
+
+}
